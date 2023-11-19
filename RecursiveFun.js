a29e4f105f39bae84a87bbc4e7d7429760c96c38
Faruk <faruk@alphacommerz.com>
2023-11-19 11:36:44 +0600

    feat: recursice function problem solving

--- RecursiveFun.js
+++ RecursiveFun.js
@@ -8,3 +8,17 @@ function factorial(n){
 
 const result =  factorial(6)
 console.log(result)
+
+//Sum of Digits:
+// Write a recursive function to find the sum of digits of a positive integer.
+
+function sumDigits(n){
+    if(n < 10){
+        return n
+    }else{
+        return (n%10) + sumDigits(Math.floor(n/10))
+    }
+}
+
+let sumResult = sumDigits(12345)
+console.log(sumResult)
