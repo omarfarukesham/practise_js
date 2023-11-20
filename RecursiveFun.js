7c10925f6b05ac9be840a230d00093739c9fb3f1
Faruk <faruk@alphacommerz.com>
2023-11-20 10:35:03 +0600

    feat: recursive multi dymentional array sum

--- RecursiveFun.js
+++ RecursiveFun.js
@@ -1,24 +1,46 @@
+/*Power Function:
+Implement a recursive function to calculate the power of a number (x^n) */
+
+function powerFun(x,n){
+    if(n === 0){
+        return 1
+    }else{
+        return x*powerFun(x, n-1)
+    }
+}
+
+let getResult = powerFun(3,4)
+// console.log(getResult)
+
+//factorial of recursive function
+
 function factorial(n){
     if(n == 0 || n == 1){
         return 1
     }else{
-        return n*factorial(n-1)
+        return n * factorial(n-1)
     }
 }
 
-const result =  factorial(6)
-console.log(result)
+const getResult2 = factorial(5)
+// console.log(getResult2)
 
-//Sum of Digits:
-// Write a recursive function to find the sum of digits of a positive integer.
+//sum this nested array by using recursive
 
-function sumDigits(n){
-    if(n < 10){
-        return n
-    }else{
-        return (n%10) + sumDigits(Math.floor(n/10))
-    }
-}
 
-let sumResult = sumDigits(12345)
-console.log(sumResult)
+// Example of recursively traversing a nested array
+function sumArray(arr) {
+   let sum = 0;
+   for(let i = 0; i < arr.length; i++){
+        if(Array.isArray(arr[i])){
+            sum += sumArray(arr[i])
+        }else{
+            sum += arr[i]
+        }
+   }
+   return sum 
+  }
+  
+  let numbers = [1, 2, [3, 4], [5, [6, 7]]];
+  let result = sumArray(numbers);
+  console.log(result); // Output: 28
